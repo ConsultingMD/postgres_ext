b6bdf623088ae4b19f0bc04b500bc8e4db6583a8
Bharat Gupta <bindassbharat311@gmail.com>
2018-05-11 16:05:08 +0530

    fix for rails 5.2

--- lib/postgres_ext/active_record/relation/query_methods.rb
+++ lib/postgres_ext/active_record/relation/query_methods.rb
@@ -189,8 +189,8 @@ module ActiveRecord
       self
     end
 
-    def build_arel_with_extensions
-      arel = build_arel_without_extensions
+    def build_arel_with_extensions(aliases = nil)
+      arel = build_arel_without_extensions(aliases)
 
       build_with(arel)
 
